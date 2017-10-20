fef75264c5fdda4dc5ad69c344335a91ebb30bdf
jgrando <john.grando@gmail.com>
2017-10-20 10:37:54 -0400

    Updated program to take multiple pages of data and ran

--- project3scraper.py
+++ project3scraper.py
@@ -11,69 +11,74 @@ def scrape_site():
     locations = [("New+York%2C+NY","NYC"), ("Los+Angeles%2C+CA","LAN"), ("San+Diego%2C+CA", "SAD"), ("Chicago%2C+IL","CHI"), ("Houston%2C+TX","HOU"), ("Phoenix%2C+AZ", "PHO"), ("Philadelphia%2C+PA", "PHI"), ("San+Antonio%2C+TX", "SAN"), ("Dallas%2C+TX", "DAL"), ("San+Jose%2C+CA", "SAJ")]
     #locations = [("New+York%2C+NY","NY")]
     index = 0
-    drop_l = ["new","york", "ny", "nyc", "los", "angeles", "ca", "chicago", "il", "houston", "tx", "phoenix", "az", "philadelphia", "pa", "san", "antonio", "diego", "dallas", "jose", "desired","30","days","ago","upload", "your", "resume", "sexual", "gender", "identity", "orientation" ,"equal", "opportunity", "employer" , "minority", "jobs", "job" , "full", "time", "female", "disability", "affirmative", "action", "contact", "us", "race", "color", "e", "g", "emploment", "without", "regard", "veteran", "veterans", "diversity", "applicants", "discrimination", "sex", "u", "s", "united", "states", "military", "i", "hereby", "agree", "national", "origin", "marital", "status", "learn", "more", "view"]
+    drop_l = ["new","york", "ny", "nyc", "los", "angeles", "ca", "chicago", "il", "houston", "tx", "phoenix", "az", "philadelphia", "pa", "san", "antonio", "diego", "dallas", "jose", "desired","30","days","ago","upload", "your", "resume", "sexual", "gender", "identity", "orientation" ,"equal", "opportunity", "employer" , "minority", "jobs", "job" , "full", "time", "female", "disability", "affirmative", "action", "contact", "us", "race", "color", "e", "g", "emploment", "without", "regard", "veteran", "veterans", "diversity", "applicants", "applications", "discrimination", "sex", "u", "s", "united", "states", "military", "i", "hereby", "agree", "national", "origin", "marital", "status", "learn", "more", "view"]
     global_l = []
     global_summary_d = {}
     loc_summary_d = {}
     link_check = []
+    loc_index = 2
     for location in locations:
         loc = location[0]
         #print(loc)
         full_l = []
         #try:
         loc_d = {}
-        url = 'https://www.indeed.com/jobs?as_and=data+science&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=100&l=' + loc + '&fromage=any&limit=100&sort=&psf=advsrch'
-        #print(url)
-        hdr = {'User-Agent': 'Mozilla/5.0'}
-        req = Request(url,headers=hdr)
-        page = urlopen(req)
-        soup = BeautifulSoup(page, 'html.parser')
-        links = soup.find_all('a', attrs={'data-tn-element': re.compile('jobTitle')})
-        for link in links:
-            if link['href'][0:100] not in link_check:
-                link_check.append(link['href'][0:100])    
-                #print(link['href'])
-                #print(".......")
-                #print(link_check)
-                entries_l = []
-                try:
-                    if link['href'].startswith(('/pagead','/company')):
-                        sub_url = 'https://www.indeed.com' + link['href']
-                    elif link['href'].startswith('https'):
-                        sub_url = link['href']
-                    elif link['href'].startswith('/rc'):
-                        sub_url = 'https://www.indeed.com' + link['href']
-                    else:
-                        sub_url = None
-                    req = Request(sub_url,headers=hdr)
-                    page = urlopen(req)
-                    soup = BeautifulSoup(page, 'html.parser')
-                    raw_entries = soup.find_all(['p','li'], text=True) 
-                    #print(raw_entries)
-                    #'div','span'
-                    for entry in raw_entries:
-                        try:
-                            entries_l.extend(tokenizer.tokenize(entry.get_text()))
-                            full_l.extend(tokenizer.tokenize(entry.get_text()))
-                            global_l.extend(tokenizer.tokenize(entry.get_text()))
-                        except:
-                            print(entry.get_text())
-                            print("______________")
-                            pass
-                    #print(entries_l)
-                    entries_l = [entry.lower() for entry in entries_l if entry not in nltk.corpus.stopwords.words('english')]
-                    entries_l = [entry.lower() for entry in entries_l if entry not in drop_l]
-                    print(entries_l)
-                    freqdist = [(k, v) for k, v in nltk.FreqDist(entries_l).items()]
-                    b_freqdist = [(k, v) for k, v in nltk.FreqDist(nltk.bigrams(entries_l)).items()]
-                    t_freqdist = [(k, v) for k, v in nltk.FreqDist(nltk.trigrams(entries_l)).items()]
-                    loc_d[index] = {"location":location[0], "title":link["title"], "ref":link["href"], "joblist":entries_l, "freqdist":freqdist, "b_freqdist":b_freqdist, "t_freqdist":t_freqdist}
-                    index += 1
-                    time.sleep(1)
-                except:
-                    print(link)
-                    print("______________")
-                    pass
+        for pg in range(0, loc_index):
+            if pg == 0:
+                url = 'https://www.indeed.com/jobs?as_and=data+science&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=' + loc + '&fromage=any&limit=100&sort=&psf=advsrch'
+            else:
+                url = 'https://www.indeed.com/jobs?q=data+science&l=' + loc + '&radius=50&limit=50&start=' + str(pg * 50)
+            print(url)
+            hdr = {'User-Agent': 'Mozilla/5.0'}
+            req = Request(url,headers=hdr)
+            page = urlopen(req)
+            soup = BeautifulSoup(page, 'html.parser')
+            links = soup.find_all('a', attrs={'data-tn-element': re.compile('jobTitle')})
+            for link in links:
+                if link['href'][0:100] not in link_check:
+                    link_check.append(link['href'][0:100])    
+                    #print(link['href'])
+                    #print(".......")
+                    #print(link_check)
+                    entries_l = []
+                    try:
+                        if link['href'].startswith(('/pagead','/company')):
+                            sub_url = 'https://www.indeed.com' + link['href']
+                        elif link['href'].startswith('https'):
+                            sub_url = link['href']
+                        elif link['href'].startswith('/rc'):
+                            sub_url = 'https://www.indeed.com' + link['href']
+                        else:
+                            sub_url = None
+                        req = Request(sub_url,headers=hdr)
+                        page = urlopen(req)
+                        soup = BeautifulSoup(page, 'html.parser')
+                        raw_entries = soup.find_all(['p','li'], text=True) 
+                        #print(raw_entries)
+                        #'div','span'
+                        for entry in raw_entries:
+                            try:
+                                entries_l.extend(tokenizer.tokenize(entry.get_text()))
+                                full_l.extend(tokenizer.tokenize(entry.get_text()))
+                                global_l.extend(tokenizer.tokenize(entry.get_text()))
+                            except:
+                                print(entry.get_text())
+                                print("______________")
+                                pass
+                        #print(entries_l)
+                        entries_l = [entry.lower() for entry in entries_l if entry not in nltk.corpus.stopwords.words('english')]
+                        entries_l = [entry.lower() for entry in entries_l if entry not in drop_l]
+                        print(entries_l)
+                        freqdist = [(k, v) for k, v in nltk.FreqDist(entries_l).items()]
+                        b_freqdist = [(k, v) for k, v in nltk.FreqDist(nltk.bigrams(entries_l)).items()]
+                        t_freqdist = [(k, v) for k, v in nltk.FreqDist(nltk.trigrams(entries_l)).items()]
+                        loc_d[index] = {"location":location[0], "title":link["title"], "ref":sub_url, "joblist":entries_l, "freqdist":freqdist, "b_freqdist":b_freqdist, "t_freqdist":t_freqdist}
+                        index += 1
+                        time.sleep(1)
+                    except:
+                        print(link)
+                        print("______________")
+                        pass
         links_df = pd.DataFrame.from_dict(loc_d, orient="index")
         links_df.to_csv("./links_data_" + location[1] + ".csv", index = False)
         #except
